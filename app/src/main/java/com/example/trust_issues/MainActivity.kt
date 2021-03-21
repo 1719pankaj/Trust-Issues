4647afcb3735588f810c5b69891c36c88c55cc17
WS <1719pankaj@gmail.com>
2021-03-21 15:37:28 +0530

    Base

--- app/src/main/java/com/example/trust_issues/MainActivity.kt
+++ app/src/main/java/com/example/trust_issues/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.trust_issues
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
+import javax.sql.RowSetListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button: Button = findViewById(R.id.button)
+        button.text = getString(R.string.demand)
+        button.setOnClickListener {
+            getPermission()
+        }
+
     }
+    private fun getPermission() {
+
+    }
+
 }
