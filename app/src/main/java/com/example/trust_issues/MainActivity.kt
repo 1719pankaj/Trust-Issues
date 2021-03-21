c5b0883ecd0d1d758ce65d49e7feef5a6d620dd1
WS <1719pankaj@gmail.com>
2021-03-21 14:48:54 +0530

    Initial Build

---
+++ app/src/main/java/com/example/trust_issues/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trust_issues
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
